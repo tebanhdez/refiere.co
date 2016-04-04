7e0d8ffe7b72aa070eb7ccb3774bd110c14ed590
Kevin Escobar Miranda <kescobar@pernix-solutions.com>
2016-04-04 15:22:46 -0600

    angular app added

---
+++ src/main/webapp/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name refiereApp
+ * @description
+ * # refiereApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('refiereApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
