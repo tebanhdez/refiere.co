7fbaa065102e98362a925e7706dda7e1764be7cb
Kevin Escobar Miranda <kescobar@pernix-solutions.com>
2016-04-06 18:18:37 -0600

    landingpage and dashboard partially created

---
+++ src/main/webapp/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name refiereApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the refiereApp
+ */
+angular.module('refiereApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
