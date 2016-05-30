f2b946c2ae2fdaa1f8644c10269f4e4fe71ab1a3
Deivid Ugarte Quesada <dugarte@pernix-solutions.com>
2016-05-30 15:43:05 -0600

    unit test for datbase import

--- src/main/java/co/refiere/resources/DatabaseImporterResource.java
+++ src/main/java/co/refiere/resources/DatabaseImporterResource.java
@@ -1,4 +1,4 @@
-package co.refiere.resources.dataimport;
+package co.refiere.resources;
 
 import java.io.InputStream;
 
@@ -6,6 +6,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
@@ -19,10 +21,11 @@ public class DatabaseImporterResource {
   @POST
   @Path("/import")
   @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public void insertDataInDB2(@FormDataParam("file") InputStream uploadedInputStream,
+  public Response insertDataInDB2(@FormDataParam("file") InputStream uploadedInputStream,
       @FormDataParam("file") FormDataContentDisposition fileDetail) {
     DataImporterService dataImporterService = new DataImporterService();
     dataImporterService.importDatabase(uploadedInputStream, fileDetail);
+    return Response.ok().build(); 
     }
 
   
