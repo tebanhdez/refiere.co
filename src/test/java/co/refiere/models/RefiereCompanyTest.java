beca54466bb2730d9fdd68f8da701f0aaf5a199f
Esteban Hernandez <Esteban.Hernandez@flatironssolutions.com>
2016-06-03 18:27:15 -0600

    Company test fixed

--- src/test/java/co/refiere/models/RefiereCompanyTest.java
+++ src/test/java/co/refiere/models/RefiereCompanyTest.java
@@ -1,27 +1,38 @@
 package co.refiere.models;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import co.refiere.dao.RefiereCompanyDao;
-import co.refiere.dao.RefiereUserDao;
-import co.refiere.resources.util.HibernateUtil;
 
 public class RefiereCompanyTest {
 
+    RefiereCompanyDao companyDao = new RefiereCompanyDao();
+    int testCompanyId;
+
+    @Before
+    public void createTestData(){
+        Company testCompany = new Company();
+        testCompany.setName("RefiereCompanyTest::testCreateRefiereCompany");
+        testCompany.setEmail("test@email.com");
+        testCompany.setAddress("Address");
+        testCompany.setPhone("+506 8989 8989");
+        companyDao.save(testCompany);
+        testCompanyId = testCompany.getId();
+    }
 
     @Test
     public void testCreateRefiereCompany() {
+        Company testCompany = companyDao.findCompanyById(testCompanyId);
+        Assert.assertNotNull("Company Not saved", testCompany);
+        Assert.assertNotNull("Invalid company", testCompany.getId());
+    }
 
-        Company newCompany = new Company();
-        //TODO: Provide Random integer - modify RefiereUser.dbm.xml to assign
-        //newUser.setId();
-        newCompany.setName("RefiereCompanyTest::testCreateRefiereCompany");
-        newCompany.setEmail("test@email.com");
-
-        RefiereCompanyDao companyDao = new RefiereCompanyDao();
-        companyDao.save(newCompany);
+    @After
+    public void deleteTestCompany(){
+        Company testCompany = companyDao.findCompanyById(testCompanyId);
+        companyDao.deleteCompany(testCompany);
     }
 }
