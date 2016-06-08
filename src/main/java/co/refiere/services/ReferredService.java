9038956c775e136e27511914b2a1be337ca0a443
Deivid Ugarte <dugarte@pernix-solutions.com>
2016-06-08 17:35:55 -0600

    changes in process

--- src/main/java/co/refiere/services/ReferredService.java
+++ src/main/java/co/refiere/services/ReferredService.java
@@ -8,21 +8,14 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.tool.schema.internal.exec.GenerationTargetToScript;
 
+import co.refiere.dao.ReferencesCodesDao;
 import co.refiere.models.ReferencesCodes;
 import co.refiere.resources.base.ReferredObjectData;
 
 public class ReferredService {
     private static final Log LOGGER = LogFactory.getLog(ReferredService.class);
 
-    public java.util.List<ReferredObjectData> getAllReferred() throws NoContentException{
-        ReferencesCodesDao referecesCodesDao = ReferencesCodesDao();
-        List<ReferredObjectData> referrals = getSimplifiedReferred(referecesCodesDao.findAllReferred());
-        if(referrals == null){
-            LOGGER.error("Referrals not found");
-            throw new NoContentException("\"Error \": \"Referrals not found\"");
-        }
-        return referrals;
-    } 
+    
 
 
 }
