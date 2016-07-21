45da914ad41f771463c120c2de45f92b81a67de6
Fernando Sánchez L <fernandojsl04@gmail.com>
2016-07-21 10:04:57 -0600

    Fix database save and send email

--- src/main/java/co/refiere/models/DefaultPrize.java
+++ src/main/java/co/refiere/models/DefaultPrize.java
@@ -4,16 +4,22 @@ package co.refiere.models;
  * Created by Esteban H.
  */
 public enum DefaultPrize {
-    MONEY       (10),
-    DISCOUNT    (11),
-    BONUS       (12),
-    PRIZE       (13),
-    UPGRADE     (14);
+    MONEY       (10, "MONEY"),
+    DISCOUNT    (11, "DISCOUNT"),
+    BONUS       (12, "BONUS"),
+    PRIZE       (13, "PRIZE"),
+    UPGRADE     (14, "UPGRADE");
 
     private int id;
-
-    DefaultPrize(int id){
+    private String prizeName;
+    
+    DefaultPrize(int id, String prizeName) {
         this.id = id;
+        this.prizeName = prizeName;
+    }
+
+    public String getPrizeName() {
+        return prizeName;
     }
 
     public int getPrizeId(){
